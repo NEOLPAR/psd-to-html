198bbb353c7753de4ae4a436c25883b72ee1ccf0
NEOLPAR <ricardo.berrocal@opendeusto.es>
2014-11-20 14:27:53 +0000

    Validation

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$( document ).ready(function() {
+  $("#submitbtn").click(function () {
+
+    var form = $('#signupForm');
+    var isValid = form.valid();
+    if (isValid) {
+      //TODO: registration
+    }
+  });
+});
